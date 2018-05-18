0ad044b53b8ecc83310cd0c65c3e6f7e907caf12
Noah Alderton <nalderto@purdue.edu>
2018-05-18 19:44:12 -0400

    Start and reset buttons work now

--- timer.js
+++ timer.js
@@ -1,26 +1,34 @@
-var timerIsRunning = false;
 var timerInterval;
+var minutes = 0;
+var seconds = 0;
+var lastMinutes = 0;
+var lastSeconds = 0;
 
-function start_stop(){
-    if(!timerIsRunning){
+
+function start(){
         var startTime = Date.now();
         timerInterval = setInterval(function(){updateClock(startTime)}, 100);
-        timerIsRunning = true;
-    }
-    else if(timerIsRunning){
-        clearInterval(timerInterval);
-        timerIsRunning = false;
-    }
+        document.getElementById("startButton").setAttribute("onClick", "javascript: stop()");
+}
+
+function stop(){
+    lastSeconds = seconds;
+    lastMinutes = minutes;
+    clearInterval(timerInterval);
+    document.getElementById("startButton").setAttribute("onClick", "javascript: start()");
+}
+
+function reset() {
+    lastMinutes = 0;
+    lastSeconds = 0;
+    document.getElementById("clock").innerHTML = `0:00`;
 }
 
 function updateClock(startTime){
-    console.log(startTime);
     var change = Date.now() - startTime;
-    console.log(change);
     var secondsDiff = Math.floor(change / 1000);
-    console.log(secondsDiff);
-    var seconds = secondsDiff % 60;
-    var minutes = Math.floor(secondsDiff / 60);
+    seconds = lastSeconds + (secondsDiff % 60);
+    minutes = lastMinutes + (Math.floor(secondsDiff / 60));
     var secondsString = "";
     if (seconds < 10){
         secondsString = "0"+seconds;
